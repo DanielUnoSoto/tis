16204b63c93e07f34675b665d9e1c589aa759c7e
adrikar <orellanakaren30@gmail.com>
2021-04-15 14:01:37 -0400

    home

---
+++ cotizacion/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark item-dark">
+    <div class="collapse navbar-collapse">
+        <a href="/" class="navbar-brand">
+            <img src="imagenes/umss.png" atl="" class="d_inline-block aling-top" height="80"
+            width="100">
+        Sistema de Cotizaciones
+        </a>
+        </div>
+    <li class="nav-item ">
+        <a class="nav-link active" aria-current="page" href="home">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="login">Login</a>
+    </li>       
+</nav>
+<div>
+    <h2>bienvenido al sistema de cotizaciones</h2>
+
+
+</div>
