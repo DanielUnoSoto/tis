2e3ae432ca9994df71b922d3413c0484e4f51a22
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2021-04-22 21:51:49 -0400

    se agrego los estilos de la pagina de registro

--- cotizacion/resources/views/auth/register.blade.php
+++ cotizacion/resources/views/auth/register.blade.php
@@ -1,5 +1,17 @@
-@extends('auth.registerlay')
-@section('content')
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!--Ultima version de fotawesome-->
+<script src="https://kit.fontawesome.com/f5ab984d8e.js" crossorigin="anonymous"></script>
+
+
+
+
+
+<link href="css/register.css" rel="stylesheet">
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="collapse navbar-collapse">
         <a href="/" class="navbar-brand">
@@ -16,35 +28,47 @@
 </nav>
 
 <br>
-<div  class="fw-bold">
-	<h2>Registro de Usuarios</h2>
+
 <br>
 <br>
-<div class="container">
-  <div class="form-check-inline">
-	<form class="row g-3">
-	<div class="col-md-4">
-		<label for="inputEmail4" class="form-label">Email</label>
-		<input type="email" class="form-control" id="inputEmail4">
+<div class="d-flex justify-content-center text-center form-register">
+  
+	
+	<form class="bg-light">
+	
+		<label class="title-inicio">
+			<h2><b>
+				Registro de Usuarios
+			</b></h2>
+
+	<div class="form-group">
+			<b><label for="inputName" class="form-label">Nombre</label></b>
+			<input type="text" class="form-control" id="inputNombres" placeholder="Ingrese sus nombres" size="40" required autofocus>
 	</div>
-	<div class="col-md-4">
-		<label for="inputPassword4" class="form-label">Contraseña</label>
-		<input type="password" class="form-control" id="inputPassword4">
+	<div class="form-group">
+		<b><label for="inputLastName" class="form-label">Apellido</label></b>
+		<input type="text" class="form-control" id="inputApellidos" placeholder="Ingrese sus apellidos" size="40" required>
 	</div>
-	<div class="col-8">
-		<label for="inputName" class="form-label">Apellido</label>
-		<input type="text" class="form-control" id="inputApellidos" placeholder="Apellidos">
+	<div class="form-group">
+		<b><label for="inputTelf" class="form-label">Teléfono</label></b>
+		<input type="tel" class="form-control" id="inputTelf" placeholder="Ingrese su número de teléfono" size="40" required>
 	</div>
-	<div class="col-8">
-		<label for="inputTelf" class="form-label">Telefono</label>
-		<input type="tel" class="form-control" id="inputTelf" placeholder="Telefono">
+	<div class="form-group">
+		<b><label for="inputEmail4" class="form-label">Correo electrónico</label></b>
+		<input type="email" class="form-control" id="inputEmail4" placeholder="Ingrese su dirección de correo" size="40" required>
 	</div>
-	<br>
-	<br>
-	<br>
-		<div class="col-md-8">
+	<div class="form-group">
+		<b><label for="inputPassword4" class="form-label">Contraseña</label></b>
+		<input type="password" class="form-control" id="inputPassword4" placeholder="Ingrese su contraseña" size="40" required>
+	</div>
+	<div class="form-group">
+		<b><label for="inputConfPassword4" class="form-label">Confirmar contraseña</label></b>
+		<input type="password" class="form-control" id="inputConfPassword4" placeholder="Confirme su contraseña" size="40" required>
+	</div>
+	
+		<div class="form-group">
 			<label for="inputRol" class="form-label">Seleccione Rol</label>
-				<select class="form-select" >
+				<select class="form-select" required>
 					<option selected></option>
 					<option value="1">Administrador</option>
 					<option value="2">Unidad Administrativa</option>
@@ -52,11 +76,11 @@
 				</select>
 
 		</div>
- 
-		</div>
-		<div class="col-12">
+		
 			<button type="submit" class="btn btn-primary">Registrar</button>
-		</div>
+		
+		
+		
 	</form>
 	</div>
-</div>
+
