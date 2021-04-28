9ee6c9293d05cc7c82c93ce0ce3f8ec1975aa39e
Daniel Uño <danielunosoto@gmail.com>
2021-04-27 23:05:50 -0400

    se aniadio un br en los index para que se vea mas bonito uwu #dus

--- cotizacion/resources/views/users/ua/index.blade.php
+++ cotizacion/resources/views/users/ua/index.blade.php
@@ -18,7 +18,7 @@
 </nav>
 <br>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Unidad Administrativa {{$user->unit->name}}</a>
+  <a class="navbar-brand" href="#">Unidad Administrativa <br> {{$user->unit->name}}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
