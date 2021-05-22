81ad025e9669873b4a0f11496ac56ff498b762ab
Daniel Uño <danielunosoto@gmail.com>
2021-05-21 21:21:39 -0400

    dificion de relaciones entre inventario,articulo y unidad #dus

--- cotizacion/app/Article.php
+++ cotizacion/app/Article.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Stock;
+
 
 class Article extends Model
 {
-    //
+	public function stock(){
+   return $this->BelongsTo(Stock::class);
+   }
 }
