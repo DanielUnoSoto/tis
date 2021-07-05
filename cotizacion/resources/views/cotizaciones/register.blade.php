044e9cd869d71701a3430d89c51a5ecc030af25f
Bryan Henry Lopez Maldonado <bryanlm71@gmail.com>
2021-07-05 12:11:13 -0400

    se arreglo el calculo de la tabla con su total

--- cotizacion/resources/views/cotizaciones/register.blade.php
+++ cotizacion/resources/views/cotizaciones/register.blade.php
@@ -3,6 +3,7 @@
 @section('mycontent')
 <link href="../css/login.css" rel="stylesheet">
 <div class="d-flex justify-content-center containerSolicitudCompleto">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<form method="POST" action="{{route('cotizaciones.store', ['petition_id' => $petition->id])}}">
 <div>
 	<div >
@@ -63,78 +64,92 @@
 		</div>
 	</div>
 		<div class="justify-content-center text-center">
-			<button type="button" class="btn btn-primary  rounded-pill" onclick="agregarFila()"><i class="fas fa-plus-square"></i> Agregar fila</button>
-			<button type="button" class="btn btn-primary  rounded-pill" onclick="eliminarFila()"><i class="fas fa-minus-square"></i> Eliminar fila</button>  
-			<table class="miTabla">
+		<table class="miTabla">
 				<thead>
 				  <tr>
 					<th>Cantidad</th>
 					<th>Unidad</th>
 					<th>Detalle</th>
-					<th>Valor unitario</th>
-					<th>Valor total</th>
+					<th nowrap>Valor unitario</th>
+					<th nowrap>Valor total</th>
 				  </tr>
 				</thead>
-				<tbody>
-					<tr>
-					  <td> <input type="text" name="quantity[]" id="quantity" size="20" class="monto" onkeyup="multi();" required autofocus> </td>
-					  <td> <input type="text" name="type_unit[]" id="type_unit" size="20" required autofocus> </td>
-					  <td> <input type="text" name="details[]" id="details" size="20" required autofocus> </td>
-					  <td> <input type="text" name="unit_value[]" id="unit_value" size="20" class="monto" onkeyup="multi();" required autofocus> </td>
-					  <td> <input type="text" name="total_value[]" id="total_value" size="20"> </td>
-					</tr>
+				<tbody id="contenedor">
+					@foreach ($petition->acquisitions as $acquisition)
+					<tr id="datos2">
+						<td> <input type="text" name="quantity[]" id="quantity" size="20" required autofocus class="cantidad" readonly value="{{$acquisition->quantity}}"></td>
+						<td> <input type="text" name="type_unit[]" id="type_unit" size="20" required autofocus readonly value="{{{$acquisition->unit_type}}}"></td>
+						<td> <input type="text" name="details[]" id="details" size="20" required autofocus> </td>
+						<td> <input   contenteditable type="text" name="unit_value[]" id="unit_value" size="20" required autofocus class="preuni"></td>
+						<td> <input class="total" type="text" name="total_value[]" id="total_value" size="20" readonly> </td>
+					  </tr>	
+					@endforeach
 				</tbody>
 			</table>
-			<div class="form-group">
-				<label for="total" >Total: </label>
-				<input class= "form-control"type="text" name="total" id="total" size="40">
+			<br>
+			<div>
+				<b>Total:</b> 
+				<span id="gran-total" class= "form-control" type="text" name="total" id="gran-total" size="40">0</span>
 			</div>
+			
 			<script>
-				function multi(){
-				 var total = 1;
-				 var change= false; //
-				 $(".monto").each(function(){
-					 if (!isNaN(parseFloat($(this).val()))) {
-						 change= true;
-						 total *= parseFloat($(this).val());
-					 }
-				 });
-				 // Si se modifico el valor , retornamos la multiplicación
-				 // caso contrario 0
-				 total = (change)? total:0;
-				 document.getElementById('total_value').value = total;
-			 }
-			 </script>
-			 <script>
-				var myTable = document.querySelector(".miTabla"); 
-					 function agregarFila(){ 
-					  var row = myTable.insertRow(myTable.rows.length);
-					  var cell1 = row.insertCell(0);
-					  var cell2 = row.insertCell(1);
-					  var cell3 = row.insertCell(2);
-					  var cell4 = row.insertCell(3);
-					  var cell5 = row.insertCell(4);
-					  cell1.innerHTML = '<input type="text" name="quantity[]" id="quantity" size="20" class="monto" onkeyup="multi();" required autofocus>';
-					  cell2.innerHTML = '<input type="text" name="type_unit[]" id="type_unit" size="20" required autofocus>';;
-					  cell3.innerHTML = '<input type="text" name="details[]" id="details" size="20" required autofocus>';;
-					  cell4.innerHTML = '<input type="text" name="unit_value[]" id="unit_value" size="20" class="monto" onkeyup="multi();" required autofocus>';;
-					  cell5.innerHTML = '<input type="text" name="total_value[]" id="total_value" size="20">';;
-					 }
+				$(document).ready(function(e) {
+				//Variables
+				var html = `
+					<tr>
+								<td><input type="text" name="cantidad[]" class="cantidad" value="0"></td>
+								<td><input type="text" name="preuni[]" class="preuni" value="0"></td>
+								<td><input type="text" name="total[]" class="total" value="0" readonly></td>
+					</tr>
+				`;
+				var maxrows = 15;
+				var x = 1;
+				// Asignar evento a cantidad y precio unitario
+				$(document).on("change", ".cantidad", multiplicar);
+				$(document).on("change", ".preuni", multiplicar);
+
+				//agregar filas al form
+				$("#add").click(function(e) {
+					// Evitar que se agreguen más filas que las especificadas en maxrows
+					if(x < maxrows) {
+						$("#contenedor").append(html);
+						x ++;
+					}
+				});
+
+				//remover filas del form
+				$(document).on('click', '.remove', function(e) {
+					$(this).parent('tr').remove();
+					x --;
+					calcularTotal();
+				});
+			});
+
+			function multiplicar(e) {
+				//datos=$("#datos2");
+				let p = $(e.target).closest('tr');
+				// Obtener contenedor desde el elemento que cambió
+			// let p = datos.closest('tr');
 				
-					 function eliminarFila(){
-					  var rowCount = myTable.rows.length;
-					  if(rowCount <= 1) {
-						alert('No se puede eliminar el encabezado');
-					  } else {
-						myTable.deleteRow(rowCount -1);
-					  }
-				  
-					 }
-			  </script>
-			 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+				// Usar .find() para obtener cantidad, precio y total
+				let m1 = parseFloat($(p).find(".cantidad").val()) || 0;
+				let m2 = parseFloat($(p).find(".preuni").val()) || 0;
+				// Asignar cálculo al campo total
+				$(p).find(".total").val(m1 * m2);
+				calcularTotal();
+			}
+
+			function calcularTotal() {
+				// Inicializar total
+				let total = 0;
+				// Recorrer totales para sumar
+				$.each($('.total'), (index, item) => total += parseInt($(item).val()) || 0);
+				// Mostrar total
+				$('#gran-total').text(total);
+			}
+			 </script>
     	</div>
-    	
-		
+    		
 	</form>
 	</div>
 	</div>
