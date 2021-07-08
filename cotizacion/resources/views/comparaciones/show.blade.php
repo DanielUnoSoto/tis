3d1fb7f9f66b5b5290f74f422988a4d59817af30
adrikar <orellanakaren30@gmail.com>
2021-07-07 22:07:19 -0400

    Cuadro comparativo

--- cotizacion/resources/views/comparaciones/show.blade.php
+++ cotizacion/resources/views/comparaciones/show.blade.php
@@ -4,34 +4,34 @@
 
 <h2 class= "transformacion1 text-center font-weight-bold">Cuadro comparativo</h2>
 <div class="container">
-	<div class="card border-success mb-3">
+	<div class="card border-dark mb-3">
 		<div class="card-body">
-			<p class="font-weight-bold" style = "float: left"> ID solicitud:</p>
-			<p>&nbsp {{$petition->id}} </p>
-			<p class="font-weight-bold" style = "float: left"> Emision: </p>
-			<p>&nbsp{{$petition->created_at}} </p>
-			<p class="font-weight-bold" style = "float: left">Unidad solicitante:</p>
-			<p class="text-capitalize"> &nbsp{{$petition->unit->name}} </p>
-			<p class="font-weight-bold" style = "float: left">Nombre solicitante:</p>
-			<p class="text-capitalize">&nbsp {{$petition->user->name}} {{$petition->user->last_name}} </p>
+			<div class="row align-items-start">
+    			<div class="col">
+					<p class="font-weight-bold" style = "float: left"> ID solicitud:</p>
+					<p>&nbsp {{$petition->id}} </p>
+				</div>
+				<div class="col">
+					<p class="font-weight-bold" style = "float: left"> Emision: </p>
+					<p>&nbsp{{$petition->created_at}} </p>
+				</div>
+			</div>
+			<div class="row align-items-start">
+				<div class="col">
+					<p class="font-weight-bold" style = "float: left">Unidad solicitante:</p>
+					<p class="text-capitalize"> &nbsp{{$petition->unit->name}} </p>
+				</div>
+				<div class="col">
+					<p class="font-weight-bold" style = "float: left">Nombre solicitante:</p>
+					<p class="text-capitalize">&nbsp {{$petition->user->name}} {{$petition->user->last_name}} </p>
+				</div>
+			</div>
 		</div>
 	</div>
+	<br>
 	<div>
-		<p>aquisiciones de una solicitud:</p>
-		@foreach($petition->acquisitions as $acquisition)
-			<p> {{$acquisition->name}},{{$acquisition->details}},{{$acquisition->unit_type}}, {{$acquisition->quantity}} </p>
-		@endforeach
-		<p>COTIZACIONES</p>
-		@foreach($quotations as $quotation)
-			<p> empresa: {{$quotation->company->name}} </p>
-			@foreach($quotation->items as $item)
-				<p> cantidad: {{$item->quantity}},valor unitario: {{$item->unit_value}} valor total: {{$item->total_value}}</p>
-			@endforeach
-			<hr>
-		@endforeach
-	</div>
-	<div>
-		<h2 class= "transformacion1 text-center font-weight-bold">Tabla comparativa</h2>
+		<h2 class= "transformacion1 font-weight-bold">Tabla comparativa</h2>
+		<br>
 		<table class="table table-hover table-bordered">
 			<thead class="thead-dark">
 				<tr>
@@ -40,92 +40,156 @@
 				<th>Detalles</th>
 				<th>Unidad</th>
 				@foreach ($quotations as $quotation)
-				<th>{{$quotation->company->name}}</th>
+				<th >{{$quotation->company->name}}</th>
 				@endforeach
 				</tr>
 			</thead>
 				<tbody>
 				@foreach ($petition->acquisitions as $acquisition)
-					<tr>
-						<td>{{$acquisition->name}}</td>
-						<td>{{$acquisition->quantity}}</td>
-						<td>{{$acquisition->details}}</td>
-						<td>{{$acquisition->unit_type}}</td>
-				
-						@foreach ($quotations as $quotation)
-							@foreach ($quotation->items as $item)
-							<td>{{$item->total_value}}</td>
-							@endforeach							
-						@endforeach
-						@endforeach	
-					</tr>
-				</tbody>
-				<tfoot>
-					<tr>
-						<td><b>TOTAL:</b></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						@foreach ($quotations as $quotation)
-							<td class="datos">{{$quotation->total}}</td>
-						@endforeach
-					</tr>
-				</tfoot>
-		</table>
-	</div>
-		<div>
-			<h2 class= "transformacion1 text-center font-weight-bold">Tabla comparativa</h2>
-			<table class="table table-hover table-bordered"  >
-			<thead class="thead-dark">
-				<tr>
-					<th>Empresa</th>
-					<th>Cantidad</th>
-					<th>Detalles</th>
-					<th>Unidad</th>
-					<th>Valor Unitario</th>
-					<th>Valor Total</th>
-				</tr>
-				<tbody>	
-				@foreach($quotations as $quotation)
 				<?php
 					foreach ($quotations as $k => $v) {
-						$tArray[$k] = $v['total_value'];
+						$tArray[$k] = $v['total'];
 					}
 					$min_value = min($tArray);
 					$max_value = max($tArray);
 				?>
-				@if($min_value == $quotation->total_value)
-				<tr class="table-success">
 				
-						<td> {{$quotation->company->name}} </td>
-						<td> {{$quotation->quantity}} </td>
-						<td> {{$quotation->details}} </td>
-						<td> {{$quotation->type_unit}} </td>
-						<td> {{$quotation->unit_value}} </td>
-						<td> {{$quotation->total_value}}  </td>
+					<tr>
+					
+						<td>{{$acquisition->name}}</td>
+						<td>{{$acquisition->quantity}}</td>
+						<td>{{$acquisition->details}}</td>
+						<td>{{$acquisition->unit_type}}</td>
+						@endforeach	
+						@foreach($quotations as $quotation)
+							@if($min_value == $quotation->total)
+							<td class="table-success" >{{$quotation->total}}</td>
+							@else
+							<td >{{$quotation->total}}</td>
+							@endif
+						@endforeach
+					
 					</tr>
-				@else
-				<tr>
-				
-						<td> {{$quotation->company->name}} </td>
-						<td> {{$quotation->quantity}} </td>
-						<td> {{$quotation->details}} </td>
-						<td> {{$quotation->type_unit}} </td>
-						<td> {{$quotation->unit_value}} </td>
-						<td> {{$quotation->total_value}}  </td>
-					</tr>
-				@endif
-					@endforeach
 				</tbody>
-			</thead>
-			</table>
+		</table>
+	</div>
+	<br>
+	<div>
+	<h3 class= "transformacion1 font-weight-bold">Cotizaciones Realizadas</h3>
+	<br>
+	@foreach($quotations as $quotation)
+	<?php
+		foreach ($quotations as $k => $v) {
+			$tArray[$k] = $v['total'];
+		}
+		$min_value = min($tArray);
+		$max_value = max($tArray);
+	?>
+	@if($min_value == $quotation->total)		
+	
+				<div  class="card border-dark  mb-3" border-width="3px">
+					
+					<div class="card-header text-white bg-success">
+						<p class="font-weight-bold" style = "float: left">Empresa: </p>
+						<p> &nbsp{{ $quotation->company_name }}</p>
+						</div>
+						
+				<div class="card-body">		
+				
+				<div>
+					<table class= "table table-hover  table-bordered"  >
+						<thead class="table-success">
+							<tr>
+								<th>N°</th>
+								<th>Cantidad</th>
+								<th>Unidad</th>
+								<th>Detalles</th>
+								<th>Precio Unitario</th>
+								<th>Precio total</th>
+							</tr>
+						</thead>
+						<tbody>	
+							@foreach($quotation->items as $item)
+								<tr>
+								<td>  </td>
+									<td> {{$item->quantity}} </td>
+									<td> {{$item->type_unit}} </td>
+									<td> {{$item->details}} </td>
+									<td> {{$item->unit_value}} </td>
+									<td> {{$item->total_value}} </td>
+								</tr>
+							@endforeach
+							<tr>
+								
+								<td colspan="5">TOTAL:</td>
+								<td >{{ $quotation->total }}</td>
+							</tr>
+						</tbody>
+					</table>
+				
+					</div>		
+				</div>
+				</div>
+			@else
+			<div  class="card border-dark mb-3">
+					
+					<div class="card-header">
+						<p class="font-weight-bold" style = "float: left">Empresa: </p>
+						<p> &nbsp{{ $quotation->company_name }}</p>
+						</div>
+						
+				<div class="card-body">		
+				
+				<div>
+					<table class= "table table-hover table-bordered"  >
+						<thead class="thead-dark">
+							<tr>
+								<th>N°</th>
+								<th>Cantidad</th>
+								<th>Unidad</th>
+								<th>Detalles</th>
+								<th>Precio Unitario</th>
+								<th>Precio total</th>
+							</tr>
+						</thead>
+						<tbody>	
+							@foreach($quotation->items as $item)
+								<tr>
+								<td>  </td>
+									<td> {{$item->quantity}} </td>
+									<td> {{$item->type_unit}} </td>
+									<td> {{$item->details}} </td>
+									<td> {{$item->unit_value}} </td>
+									<td> {{$item->total_value}} </td>
+								</tr>
+							@endforeach
+							<tr>
+								
+								<td class="table-dark" colspan="5">TOTAL:</td>
+								<td class="table-dark">{{ $quotation->total }}</td>
+							</tr>
+						</tbody>
+					</table>
+				
+					</div>		
+				</div>
+				</div>
+			@endif
+			@endforeach
+		
+		<div>
+		<br>
+		<br>
+		
 		<div class="text-center justufy-content-center">
-			<select class="form-select" aria-label=".form-select-sm example" name="">
-				<option selected></option>
+			<select class="form-select  form-control" aria-label=".form-select-lg example"name="">
+				<option selected>Selecciona a la empresa ganadora:</option>
 				@foreach($quotations as $quotation)
 					<option value="{{$quotation->company->name}}">{{$quotation->company->name}}</option>
 				@endforeach
+				
 			</select>
+		
 			<br> <br>
 			<div class="d-grid gap-2 d-md-flex justify-content-md-center">
 				<div style="padding-right: 5px">
@@ -135,25 +199,11 @@
 			</div>
 		</div>
 		</div>
-		
-</div>
-</div>
-<script>
-	//var input = document.getElementById("valorTotal").style.color="black";
-	var datos = [document.getElementsByClassName("datos").innerText];
-	let numeros = [1,2,3,4,5,10];
-	console.log(Math.min.apply(null, numeros));
-	console.log(datos);
-	var x = document.getElementsByClassName("datos");
-var y = document.getElementsByClassName("datos");
 
-for (var  i = 0; i < x.length; i++){
-  var text = y[i].innerText;
-  // text es una variable que va cambiando su valor a medida que se ejecuta el bucle
-  console.log(text);
-  console.log(Math.min(text));
-}
-var z = Math.min(text);
-  console.log(z);
+</div>
+</div>
+<script type="text/javascript">
+	var input = document.getElementById("valorTotal").style.color="black"; 				
 </script>
+
 @stop
